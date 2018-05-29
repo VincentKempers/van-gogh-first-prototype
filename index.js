dde94659d268c84ab393dd252cddb1e7b4d18981
VincentKempers <me@vncn.tk>
2018-05-29 12:01:34 +0200

    add home to /

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+  res.render('home');
+})
 
 app.listen(4044, () => {
   console.log("app runs at http://localhost:4044");
