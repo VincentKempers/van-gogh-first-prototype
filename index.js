d16947975c262d4b3a53653dc2ebf11b4cdfbbdd
Vincent Kempers <VincentKempers@users.noreply.github.com>
2018-05-30 16:57:07 +0200

    Feature/zero state (#1)
    * we tried parcel but it failed us
    
    * Removed parcel
    
    * Layout selection and demo selection of item
    
    * Removed parcel. add pm script with watchify & node-sass
    
    * add the new package & add style

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 
-
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use('/dist', express.static('dist'));
 
 app.get('/', (req, res) => {
-  res.render('home');
-})
+	res.render('home');
+});
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+app.get('/selection', (req, res) => {
+	res.render('selection');
+});
 
 app.listen(4044, () => {
-  console.log("app runs at http://localhost:4044");
-})
+	console.log('app runs at http://localhost:4044');
+});
